ef18c5259c651cff27fc213d90c197cf69c0c47c
sireesha302 <sirik302@gmail.com>
2017-02-19 12:30:47 +0530

    sample

--- hai.java
+++ hai.java
@@ -2,5 +2,7 @@ class hai{
 	public static void main(String args[])
 	{
 		System.out.println("haiiiiiiiiiiiii");
+		System.out.println("haiiiiiiiiiiiii");
+		System.out.println("haiiiiiiiiiiiii");
 	}
 }
